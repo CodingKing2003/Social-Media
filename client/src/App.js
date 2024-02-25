a14ec368a9559742348cad4b224d68a9b036c6bd
Rajan <rajanj448@student.sfit.ac.in>
2024-02-25 12:24:17 +0530

    Setup Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter,Route,Routes} from 'react-router-dom'
+import PageRender from './PageRender';
 
 function App() {
   return (
+    <BrowserRouter>
+    <input type="checkbox" id='theme' />
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main">
+      <Routes>
+      <Route path='/:page' element={<PageRender />}  />
+      <Route path='/:page/:id' element={<PageRender />}  />
+      </Routes>
+      </div>
     </div>
+    
+    </BrowserRouter>
   );
 }
 
